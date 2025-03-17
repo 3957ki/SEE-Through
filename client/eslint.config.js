069eb4955c796fef19c68288103455357ae75eab
Gwon Hong <gwonhong.dev@gmail.com>
2025-03-17 13:49:45 +0900

    :bug: Fix: tseslint plugin 설정 업데이트

--- client/eslint.config.js
+++ client/eslint.config.js
@@ -1,4 +1,4 @@
-import js from "@eslint/js";
+import eslint from "@eslint/js";
 import reactDom from "eslint-plugin-react-dom";
 import reactHooks from "eslint-plugin-react-hooks";
 import reactNamingConvention from "eslint-plugin-react-naming-convention";
@@ -31,7 +31,7 @@ export default tseslint.config(
       },
     },
     plugins: {
-      "@typescript-eslint": tseslint,
+      "@typescript-eslint": tseslint.plugin,
       "react-hooks": reactHooks,
       "react-refresh": reactRefresh,
       "react-x": reactX,
@@ -39,14 +39,15 @@ export default tseslint.config(
       "react-naming-convention": reactNamingConvention,
     },
     rules: {
-      ...js.configs.recommended.rules,
+      ...eslint.configs.recommended.rules,
       ...tseslint.configs.recommendedTypeChecked.rules,
+      "no-unused-vars": "off",
+      "@typescript-eslint/no-unused-vars": "error",
       ...reactHooks.configs.recommended.rules,
       ...reactX.configs["recommended-type-checked"].rules,
       ...reactDom.configs.recommended.rules,
       "react-refresh/only-export-components": ["warn", { allowConstantExport: true }],
-
-      // Other naming convention rules
+      // Naming convention rules
       "react-naming-convention/filename-extension": [
         "error",
         {
