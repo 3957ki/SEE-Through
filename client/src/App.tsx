81fb0e0158438a5d1b223e15dd4c976923c50404
Gwon Hong <gwonhong.dev@gmail.com>
2025-03-17 12:53:47 +0900

    :recycle: Refactor: 냉장고 분리

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,13 +2,18 @@ import Header from "@/components/layout/Header";
 import MainPage from "@/pages/MainPage";
 import Providers from "@/Providers";
 
+function FridgeDisplay() {
+  return (
+    <div className="max-w-md mx-auto bg-white min-h-screen">
+      <Header />
+      <MainPage />
+    </div>
+  );
+}
 function App() {
   return (
     <Providers>
-      <div className="max-w-md mx-auto bg-white min-h-screen">
-        <Header />
-        <MainPage />
-      </div>
+      <FridgeDisplay />
     </Providers>
   );
 }
