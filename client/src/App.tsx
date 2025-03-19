dc18e3a3e74b9dc6a2d471311bd825c91e1ba624
Gwon Hong <gwonhong.dev@gmail.com>
2025-03-19 09:37:06 +0900

    :bug: Fix: 반응형 레이아웃 수정

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,18 +2,13 @@ import Header from "@/components/layout/Header";
 import MainPage from "@/pages/MainPage";
 import Providers from "@/Providers";
 
-function FridgeDisplay() {
-  return (
-    <div className="max-w-md mx-auto bg-white min-h-screen">
-      <Header />
-      <MainPage />
-    </div>
-  );
-}
 function App() {
   return (
     <Providers>
-      <FridgeDisplay />
+      <div className="w-full h-full bg-white">
+        <Header />
+        <MainPage />
+      </div>
     </Providers>
   );
 }
