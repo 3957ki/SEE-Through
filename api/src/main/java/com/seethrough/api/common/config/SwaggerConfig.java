7f5926b845603290c5aa0e0e45100b0f2b660aee
김종헌 <kimjjong321@naver.com>
2025-03-22 18:52:22 +0900

    ✨ Feat: Swagger 접속 시 로그인해야 하도록 수정

--- api/src/main/java/com/seethrough/api/common/config/SwaggerConfig.java
+++ api/src/main/java/com/seethrough/api/common/config/SwaggerConfig.java
@@ -1,20 +1,82 @@
 package com.seethrough.api.common.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 
 @Configuration
+@EnableWebSecurity
 public class SwaggerConfig {
+
+	@Value("${swagger.username}")
+	private String username;
+
+	@Value("${swagger.password}")
+	private String password;
 
 	@Bean
 	public OpenAPI customOpenAPI() {
+		final String securitySchemeName = "basicAuth";
+
 		return new OpenAPI()
 			.info(new Info()
 				.title("SeeThrough API")
 				.description("Spring Boot로 구현한 SeeThrough API 문서입니다.")
+			)
+			.addSecurityItem(new SecurityRequirement().addList(securitySchemeName))
+			.components(new Components()
+				.addSecuritySchemes(securitySchemeName,
+					new SecurityScheme()
+						.name(securitySchemeName)
+						.type(SecurityScheme.Type.HTTP)
+						.scheme("basic")
+				)
 			);
+	}
+
+	@Bean
+	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+		http
+			.csrf(AbstractHttpConfigurer::disable)
+			.authorizeHttpRequests(authorize -> authorize
+				// Swagger UI 관련 경로만 인증 필요
+				.requestMatchers("/swagger-ui/**", "/swagger-ui.html", "/v3/api-docs/**").authenticated()
+				// 나머지 모든 경로는 인증 없이 접근 가능
+				.anyRequest().permitAll()
+			)
+			.httpBasic(Customizer.withDefaults());
+
+		return http.build();
+	}
+
+	@Bean
+	public InMemoryUserDetailsManager userDetailsService() {
+		UserDetails user = User.builder()
+			.username(username)
+			.password(passwordEncoder().encode(password))
+			.roles("ADMIN")
+			.build();
+		return new InMemoryUserDetailsManager(user);
+	}
+
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
 	}
 }
