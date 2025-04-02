9bbf768a88f29267509c7b4db2a57037622a2624
Gwon Hong <gwonhong.dev@gmail.com>
2025-04-02 09:12:59 +0900

    :wrench: Chore: try to fix dialog

--- client/src/components/Dialog.tsx
+++ client/src/components/Dialog.tsx
@@ -10,10 +10,10 @@ export function Dialog({ content, isOpen, onClose }: CustomDialogProps) {
   if (!isOpen) return null;
 
   return (
-    <div className="absolute inset-0 flex items-start justify-center z-50">
+    <div className="absolute inset-0 flex items-center justify-center">
       <div className="absolute inset-0 bg-black/20" onClick={onClose} />
       <div
-        className="relative z-10 bg-white rounded-lg p-6 max-w-[350px] shadow-lg mx-4 mt-20"
+        className="relative z-10 bg-white rounded-lg p-6 shadow-lg mx-4"
         onClick={(e) => e.stopPropagation()}
       >
         {content}
