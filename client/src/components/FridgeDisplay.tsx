f76e8db903054170c728b38e99fb5d13a97e4b61
Gwon Hong <gwonhong.dev@gmail.com>
2025-04-03 12:36:29 +0900

    :art: Style: remove strange styling

--- client/src/components/FridgeDisplay.tsx
+++ client/src/components/FridgeDisplay.tsx
@@ -48,7 +48,6 @@ const FridgeDisplay = ({
           width: "100%",
           height: "100%",
           boxShadow: "0 4px 12px rgba(0,0,0,0.1)",
-          fontSize: "calc(16px * 0.3)", // Scale down font size
         }}
       >
         <div className="w-full shrink-0">
