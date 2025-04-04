c944f9f3093e64fcf79d8d828f837d5b1a8337c1
김종헌 <kimjjong321@naver.com>
2025-04-04 16:20:47 +0900

    :bug: Fix: cors 허용 주소 변경

--- api/src/main/java/com/seethrough/api/common/config/WebConfig.java
+++ api/src/main/java/com/seethrough/api/common/config/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
 	@Override
 	public void addCorsMappings(CorsRegistry registry) {
 		registry.addMapping("/api/**")
-			.allowedOrigins("http://localhost:5173", "http://localhost:5174", "http://localhost:5175")
+			.allowedOrigins("https://j12s002.p.ssafy.io", "http://localhost:5173")
 			.allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH")
 			.allowedHeaders("Content-Type");
 	}
