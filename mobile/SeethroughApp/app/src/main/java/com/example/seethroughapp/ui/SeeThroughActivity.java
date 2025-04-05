c86a74d57170c4c1db986b70420eafa347e17b96
방민규 <bangmq@naver.com>
2025-04-05 22:06:59 +0900

    :sparkles: Feat: 식단 갱신 기능 추가

--- mobile/SeethroughApp/app/src/main/java/com/example/seethroughapp/ui/SeeThroughActivity.java
+++ mobile/SeethroughApp/app/src/main/java/com/example/seethroughapp/ui/SeeThroughActivity.java
@@ -1,6 +1,7 @@
 package com.example.seethroughapp.ui;
 
 import android.content.Intent;
+import android.graphics.drawable.GradientDrawable;
 import android.media.Image;
 import android.os.Bundle;
 import android.view.View;
@@ -10,6 +11,7 @@ import android.widget.ProgressBar;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 import androidx.lifecycle.ViewModelProvider;
 
 import com.example.seethroughapp.R;
@@ -22,10 +24,12 @@ import java.util.List;
 
 public class SeeThroughActivity extends AppCompatActivity {
 
+    private LinearLayout firstMealCard;
     private TextView firstMealType;
     private TextView firstMealContent;
     private ImageButton firstMealRefreshBtn;
     private ProgressBar firstMealProgress;
+    private LinearLayout secondMealCard;
     private TextView secondMealType;
     private TextView secondMealContent;
     private ImageButton secondMealRefreshBtn;
@@ -58,24 +62,26 @@ public class SeeThroughActivity extends AppCompatActivity {
             }
         });
 
+        firstMealCard = findViewById(R.id.card_first);
         firstMealType = findViewById(R.id.tv_card_first_title);
         firstMealContent = findViewById(R.id.tv_card_first_content);
         firstMealRefreshBtn = findViewById(R.id.btn_first_refresh);
         firstMealRefreshBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                viewModel.refreshFirstMeal();
             }
         });
         firstMealProgress = findViewById(R.id.progress_first_meal);
 
+        secondMealCard = findViewById(R.id.card_second);
         secondMealType = findViewById(R.id.tv_card_second_title);
         secondMealContent = findViewById(R.id.tv_card_second_content);
         secondMealRefreshBtn = findViewById(R.id.btn_second_refresh);
         secondMealRefreshBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                viewModel.refreshSecondMeal();
             }
         });
         secondMealProgress = findViewById(R.id.progress_second_meal);
@@ -86,8 +92,50 @@ public class SeeThroughActivity extends AppCompatActivity {
         viewModel = new ViewModelProvider(this).get(SeeThroughViewModel.class);
         viewModel.getIngredients().observe(this, this::displayIngredients);
         viewModel.getInOutLogs().observe(this, this::displayInOutLogs);
+
         viewModel.getFirstMeal().observe(this, this::displayFirstMeal);
+        viewModel.getIsFirstMealLoading().observe(this, isLoading -> {
+            GradientDrawable drawable = (GradientDrawable) ContextCompat.getDrawable(this, R.drawable.rounded_card);
+            if (isLoading){
+                if (drawable != null){
+                    drawable.setColor(ContextCompat.getColor(this, R.color.st_dark_gray));
+                    firstMealCard.setBackground(drawable);
+                }
+                firstMealRefreshBtn.setEnabled(false);
+                firstMealProgress.setVisibility(View.VISIBLE);
+                firstMealContent.setVisibility(View.INVISIBLE);
+
+            } else {
+                if (drawable != null){
+                    drawable.setColor(ContextCompat.getColor(this, R.color.st_white));
+                    firstMealCard.setBackground(drawable);
+                }
+                firstMealRefreshBtn.setEnabled(true);
+                firstMealProgress.setVisibility(View.GONE);
+                firstMealContent.setVisibility(View.VISIBLE);
+            }
+        });
         viewModel.getSecondMeal().observe(this, this::displaySecondMeal);
+        viewModel.getIsSecondMealLoading().observe(this, isLoading -> {
+            GradientDrawable drawable = (GradientDrawable) ContextCompat.getDrawable(this, R.drawable.rounded_card);
+            if (isLoading) {
+                if (drawable != null){
+                    drawable.setColor(ContextCompat.getColor(this, R.color.st_dark_gray));
+                    secondMealCard.setBackground(drawable);
+                }
+                secondMealRefreshBtn.setEnabled(false);
+                secondMealProgress.setVisibility(View.VISIBLE);
+                secondMealContent.setVisibility(View.INVISIBLE);
+            } else {
+                if (drawable != null){
+                    drawable.setColor(ContextCompat.getColor(this, R.color.st_white));
+                    secondMealCard.setBackground(drawable);
+                }
+                secondMealRefreshBtn.setEnabled(true);
+                secondMealProgress.setVisibility(View.GONE);
+                secondMealContent.setVisibility(View.VISIBLE);
+            }
+        });
         viewModel.fetchMeals();
         viewModel.fetchIngredients(10);
         viewModel.fetchInOutLogs(10);
@@ -95,7 +143,7 @@ public class SeeThroughActivity extends AppCompatActivity {
 
     private void displayIngredients(List<Ingredient> data) {
         StringBuilder ingredientString = new StringBuilder();
-        if (data != null && !data.isEmpty()){
+        if (data != null && !data.isEmpty()) {
             int maxCnt = Math.min(5, data.size());
             for (int i = 0; i < maxCnt; i++)
                 ingredientString.append(data.get(i).getName()).append(" ");
