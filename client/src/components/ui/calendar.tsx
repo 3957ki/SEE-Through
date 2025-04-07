14ec68295258ba2c9d2c2c82842348adcd5436ac
Gwon Hong <gwonhong.dev@gmail.com>
2025-04-07 14:18:07 +0900

    :sparkles: Feat: fix calendar

--- client/src/components/ui/calendar.tsx
+++ client/src/components/ui/calendar.tsx
@@ -2,14 +2,12 @@ import { cn } from "@/lib/utils";
 import { format } from "date-fns";
 import { ko } from "date-fns/locale";
 import { DayPicker } from "react-day-picker";
-import "react-day-picker/dist/style.css";
 
 type CalendarProps = React.ComponentProps<typeof DayPicker>;
 
 export function Calendar({ ...props }: CalendarProps) {
   return (
     <DayPicker
-      animate
       locale={ko}
       showOutsideDays
       captionLayout="dropdown"
