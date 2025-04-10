6ee9c4624b084ad1645f1749adadded4c2de18dd
Gwon Hong <gwonhong.dev@gmail.com>
2025-04-10 23:20:58 +0900

    :sparkles: Feat: lint error fix

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,50 +1,12 @@
 import FridgeDisplay from "@/components/FridgeDisplay";
 import ShowcaseScreen from "@/components/ShowcaseScreen";
-import { Button } from "@/components/ui/button";
 import { CurrentMemberIdProvider } from "@/contexts/CurrentMemberIdContext";
 import { DialogProvider } from "@/contexts/DialogContext";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import React, { useRef, useState } from "react";
-
-function ShowcaseToggleButton({ onClick, title }: { onClick: () => void; title: string }) {
-  return (
-    <Button
-      variant="outline"
-      size="icon"
-      onClick={onClick}
-      className="fixed z-50 flex items-center justify-center"
-      style={{
-        bottom: "2vh",
-        right: "7vh",
-        width: "4vh",
-        height: "4vh",
-        fontSize: "2vh",
-      }}
-      title={title}
-    >
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        viewBox="0 0 24 24"
-        fill="none"
-        stroke="currentColor"
-        strokeWidth="2"
-        strokeLinecap="round"
-        strokeLinejoin="round"
-        style={{
-          width: "2vh",
-          height: "2vh",
-        }}
-      >
-        <rect x="2" y="3" width="20" height="14" rx="2" ry="2"></rect>
-        <line x1="8" y1="21" x2="16" y2="21"></line>
-        <line x1="12" y1="17" x2="12" y2="21"></line>
-      </svg>
-    </Button>
-  );
-}
+import React, { useRef } from "react";
 
 function App() {
-  const [isShowcase, setIsShowcase] = useState(true);
+  // const [isShowcase, setIsShowcase] = useState(true);
   const fridgeDisplayRef = useRef<HTMLDivElement>(null);
 
   const queryClient = new QueryClient();
@@ -52,11 +14,7 @@ function App() {
   return (
     <QueryClientProvider client={queryClient}>
       <CurrentMemberIdProvider>
-        <AppContent
-          isShowcase={isShowcase}
-          setIsShowcase={setIsShowcase}
-          fridgeDisplayRef={fridgeDisplayRef}
-        />
+        <AppContent isShowcase={true} fridgeDisplayRef={fridgeDisplayRef} />
       </CurrentMemberIdProvider>
     </QueryClientProvider>
   );
@@ -64,11 +22,9 @@ function App() {
 
 function AppContent({
   isShowcase,
-  setIsShowcase,
   fridgeDisplayRef,
 }: {
   isShowcase: boolean;
-  setIsShowcase: React.Dispatch<React.SetStateAction<boolean>>;
   fridgeDisplayRef: React.RefObject<HTMLDivElement | null>;
 }) {
   return (
@@ -92,10 +48,6 @@ function AppContent({
           </div>
         </div>
       )}
-      {/* <ShowcaseToggleButton
-        onClick={() => setIsShowcase((prev) => !prev)}
-        title="toggle showcase"
-      /> */}
     </DialogProvider>
   );
 }
